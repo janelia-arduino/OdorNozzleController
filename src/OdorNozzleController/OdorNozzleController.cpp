34e62803a8d00ddcb906e6a808ecb17ff7d71d25
Peter Polidoro <peterpolidoro@gmail.com>
2020-09-04 16:29:23 -0400

    Update to work with ArduinoJson 6

--- src/OdorNozzleController/OdorNozzleController.cpp
+++ src/OdorNozzleController/OdorNozzleController.cpp
@@ -237,8 +237,8 @@ void OdorNozzleController::homedHandler(size_t channel)
 // floating-point number (float, double)
 // bool
 // const char *
-// ArduinoJson::JsonArray *
-// ArduinoJson::JsonObject *
+// ArduinoJson::JsonArray
+// ArduinoJson::JsonObject
 // const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
